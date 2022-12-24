e5adc2555b640d8b2d8a4682de043f6e0d3490cf
chandan <chandantomar98@gmail.com>
2022-12-24 11:45:23 +0530

    swagger

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
-
 const express = require('express')
 const app = express()
+const swaggerUi = require('swagger-ui-express');
+
+
+const YAML = require('yamljs');
+const swaggerDocuments = YAML.load('./swagger.yaml');
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocuments));
+
 const port = process.env.PORT;
 
 app.get('/', (req, res) => res.send('Hello World!'))
